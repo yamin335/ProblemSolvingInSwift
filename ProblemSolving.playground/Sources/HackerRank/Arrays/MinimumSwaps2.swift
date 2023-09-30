3b3ee00b5bfb1b7bd7250b1c61af4df4dcbbff43
Yamin <myamin335@gmail.com>
2023-09-30 18:53:19 +0200

    Array minimum swaps 2 problem solved

--- ProblemSolving.playground/Sources/HackerRank/Arrays/MinimumSwaps2.swift
+++ ProblemSolving.playground/Sources/HackerRank/Arrays/MinimumSwaps2.swift
@@ -1,2 +1,21 @@
-import Foundation
+func minimumSwaps(arr: [Int]) -> Int {
+    var minSwap = 0
+    var sorted = Array(repeating: 0, count: 1) + arr
+    for i in 1...arr.count {
+        var element = sorted[i]
+        
+        if sorted[i] == i {
+            continue
+        }
+        
+        while element != i {
+            let temp = sorted[element]
+            sorted[element] = element
+            minSwap += 1
+            element = temp
+        }
+    }
+    return minSwap
+}
 
+//print(minimumSwaps(arr: [1, 3, 5, 2, 4, 6, 7,])) // Prints 3
