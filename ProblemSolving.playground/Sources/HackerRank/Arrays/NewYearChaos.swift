cfc5dfd2817c791cdaf68abbfae030cfabdb980f
Yamin <myamin335@gmail.com>
2023-09-30 18:57:48 +0200

    New year chaos problem solved

--- ProblemSolving.playground/Sources/HackerRank/Arrays/NewYearChaos.swift
+++ ProblemSolving.playground/Sources/HackerRank/Arrays/NewYearChaos.swift
@@ -1,2 +1,20 @@
-import Foundation
-
+func minimumBribes(q: [Int]) -> Void {
+    var minBribe = 0
+    var i = q.count - 1
+    while i >= 0 {
+        if (q[i] - 1) - i > 2 {
+            print("Too chaotic")
+            return
+        } else {
+            var j = max(q[i] - 2, 0)
+            while j < i {
+                if q[j] > q[i] {
+                    minBribe += 1
+                }
+                j += 1
+            }
+        }
+        i -= 1
+    }
+    print(minBribe)
+}
