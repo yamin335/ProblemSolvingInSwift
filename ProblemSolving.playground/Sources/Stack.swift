6668ca3f458cd45b1ca1016b824ebb303e10f41c
Yamin <myamin335@gmail.com>
2023-10-02 20:54:29 +0200

    Generic stack in swift

--- ProblemSolving.playground/Sources/Stack.swift
+++ ProblemSolving.playground/Sources/Stack.swift
@@ -1,2 +1,23 @@
-import Foundation
-
+struct Stack<T> {
+    private var data: [T] = []
+    
+    mutating func pop() -> T? {
+        data.popLast()
+    }
+    
+    mutating func push(item: T) {
+        data.append(item)
+    }
+    
+    func peek() -> T? {
+        data.last
+    }
+    
+    func isEmpty() -> Bool {
+        data.isEmpty
+    }
+    
+    func isNotEmpty() -> Bool {
+        !data.isEmpty
+    }
+}
