341318ee6a6b5a68ff7eae02ffc305e21c081226
Yamin <myamin335@gmail.com>
2023-10-02 20:53:55 +0200

    Generic stack in swift

--- ProblemSolving.playground/Sources/Queue.swift
+++ ProblemSolving.playground/Sources/Queue.swift
@@ -1,2 +1,23 @@
-import Foundation
-
+struct Queue<T> {
+    private var data: [T] = []
+    
+    mutating func poll() -> T? {
+        data.popLast()
+    }
+    
+    mutating func add(item: T) {
+        data.append(item)
+    }
+    
+    func peek() -> T? {
+        data.last
+    }
+    
+    func isEmpty() -> Bool {
+        data.isEmpty
+    }
+    
+    func isNotEmpty() -> Bool {
+        !data.isEmpty
+    }
+}
