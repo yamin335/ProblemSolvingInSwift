4339f5a46f259348c3e8db9775e192af915e2c74
Yamin <myamin335@gmail.com>
2023-10-02 20:53:09 +0200

    Longest common child of two string problem solved

--- ProblemSolving.playground/Sources/HackerRank/String/LongestCommonChild.swift
+++ ProblemSolving.playground/Sources/HackerRank/String/LongestCommonChild.swift
@@ -1,2 +1,21 @@
-import Foundation
-
+func commonChild(s1: String, s2: String) -> Int {
+    let str1 = Array("@\(s1)")
+    let str2 = Array("@\(s2)")
+    var previous = Array(repeating: 0, count: str1.count)
+    var present = Array(repeating: 0, count: str1.count)
+    var maxLength = Int.min
+    
+    for i in 1..<str1.count {
+        for j in 1..<str2.count {
+            if str1[i] == str2[j] {
+                present[j] = previous[j - 1] + 1
+            } else {
+                present[j] = max(present[j - 1], previous[j])
+            }
+            maxLength = max(maxLength, present[j])
+        }
+        previous = present
+        present = Array(repeating: 0, count: str1.count)
+    }
+    return maxLength
+}
